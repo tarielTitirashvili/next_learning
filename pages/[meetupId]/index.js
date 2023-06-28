c54b77cd80e53a49e73c656bbad34722a8d7757c
tarielTitirashvili <tarieltitirashvili@gmail.com>
2023-06-28 11:33:50 +0400

    initial project for meetups

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MeetuoId() {
+  return (
+    <div>MeetuoId</div>
+  )
+}
+
+export default MeetuoId
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+function Home() {
   return (
-    <main>
-      ტარიელ
-    </main>
+    <div>Home</div>
   )
 }
+
+export default Home
